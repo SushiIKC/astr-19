22e69c2c4c54df59250a86b850111267ebc260df
Isaiah <146883294+SushiIKC@users.noreply.github.com>
2023-10-10 18:15:12 -0700

    Add files via upload

---
+++ session2prompt.py
@@ -0,0 +1,5 @@
+print("13.2 + 5.3 = ", 13.2+5.3) #sum of two floating point numbers
+
+print("110 - 92 = ", 110-92) #difference between two integers 
+
+print("1928324.7362 * 49289837 = ", 1928324.7362*49289837) #product 
