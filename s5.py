687e1bbdf40fe452f70dfedf9a83ddf511433362
Isaiah <146883294+SushiIKC@users.noreply.github.com>
2023-10-31 16:39:14 -0700

    Update s5.py

--- s5.py
+++ s5.py
@@ -2,10 +2,6 @@ import numpy as np
 import math
 
 def main():
-    num_points = 1000   #defining the range for the numbers  
-    start_x = 0
-    end_x = 2 * math.pi
-
     x_values = np.linspace(0, 2 * math.pi, 1000)   #generating x values 
 
     print("x      | sin(x)")   #make a table for looks 
